14e400c5e020db7efaefa0edf2d7f1e9f6156241
evanbacon <bacon@expo.io>
2024-12-25 22:55:36 -0600

    Update expo-target.config.js

--- targets/safari/expo-target.config.js
+++ targets/safari/expo-target.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@bacons/apple-targets/app.plugin').ConfigFunction} */
 module.exports = (config) => ({
   type: "safari",
+  deploymentTarget: "18.0",
   entitlements: {
     /* Add entitlements */
   },
